b3749a25aa3380585e1374c49bd5d4bdd1a2944c
erikbirknes96-cmd <erikbirknes96@gmail.com>
2026-02-05 11:46:48 +0100

    fetch api works and info gets displayed

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-// import displayData from "https://pokeapi.co/api/v2/pokemon/ditto"
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/ditto"
-
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
 
 const display = document.querySelector("#display");
 
 async function fetchData() {
   console.log("This is fetchData function");
-  return;
+  const apiUrl = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+console.log(apiUrl);
+const response =await apiUrl.json();
+   return response;
 }
 
 async function displayData() {
   const response = await fetchData();
   console.log(response);
+
+  const h2 = document.createElement("h2")
+  h2.textContent = response.name;
+  console.log(h2)
+  display.appendChild(h2)
+
+  const sprite = document.createElement("img")
+  sprite.src = response.sprites.front_shiny;
+  console.log(sprite)
+  display.appendChild(sprite)
+
+  const baseXp = document.createElement("h3")
+  baseXp.textContent = response.base_experience
+  console.log(baseXp)
+  display.appendChild(baseXp)
+
+  const pokeId = document.createElement("li")
+  pokeId.textContent = response.id
+  console.log(pokeId)
+  display.appendChild(pokeId)
 }
 
 displayData();
